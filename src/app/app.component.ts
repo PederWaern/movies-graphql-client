61de096b6c1bec993d1802d635241996d1dc7c2b
Peder Waern <pederwaern@hotmail.com>
2018-05-02 21:09:31 +0200

    refactor config load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Apollo} from 'apollo-angular';
-import gql from 'graphql-tag';
-import {ConfigService} from './config.service';
+import {ConfigService, GET_CONFIGURATION_QUERY} from './config.service';
 import {ConfigModel} from './models/config.model';
-import {HttpLink} from "apollo-angular-link-http";
-import {InMemoryCache} from "apollo-cache-inmemory";
-import {Subscription} from "apollo-client/util/Observable";
+import {Subscription} from 'apollo-client/util/Observable';
 
 @Component({
   selector: 'app-root',
@@ -13,42 +10,21 @@ import {Subscription} from "apollo-client/util/Observable";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  private configService: ConfigService;
   public configModel: ConfigModel;
-  private apollo: Apollo;
-  private httpLink: HttpLink;
-  title = 'app';
   private querySubscription: Subscription;
 
-  constructor(apollo: Apollo, configService: ConfigService, httpLink: HttpLink) {
-    this.apollo = apollo;
-    apollo.create({
-      link: httpLink.create({ uri: 'http://localhost:8080/graphql'}),
-      cache: new InMemoryCache()
-    });
-    console.log('from constructor in appcomponent');
-    console.log(this.apollo.getClient());
+  constructor(private apollo: Apollo, private configService: ConfigService) {
   }
 
   ngOnInit() {
-    const GET_CONFIGURATION = gql`query Conf {
-      config {
-        baseUrl
-        secureBaseUrl
-        logoSizes
-        posterSizes
-        profileSizes
-        stillSizes
-        backdropSizes
-      }
-    }`;
-
+    this.configService.createApollo();
     this.querySubscription = this.apollo.watchQuery<any>({
-      query: GET_CONFIGURATION
+      query: GET_CONFIGURATION_QUERY
     })
       .valueChanges
       .subscribe(({data}) => {
         this.configModel = data.config;
+        this.configService.setConfig(this.configModel);
       });
   }
 }
--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -3,25 +3,10 @@ import {Apollo} from 'apollo-angular';
 import gql from 'graphql-tag';
 import {ConfigModel} from './models/config.model';
 import {Subscription} from 'apollo-client/util/Observable';
+import {HttpLink} from "apollo-angular-link-http";
+import {InMemoryCache} from "apollo-cache-inmemory";
 
-@Injectable()
-export class ConfigService implements OnInit {
-
-  constructor(private apollo: Apollo) {
-  }
-
-  public config: ConfigModel;
-  private querySubscription: Subscription;
-  resp: any;
-
-  ngOnInit() {
-  }
-
-  getConfig() {
-    return this.config;
-  }
-  fetchConfig(): ConfigModel {
-    const GET_CONFIGURATION = gql`query Conf {
+export const GET_CONFIGURATION_QUERY = gql`query Conf {
       config {
         baseUrl
         secureBaseUrl
@@ -33,17 +18,34 @@ export class ConfigService implements OnInit {
       }
     }`;
 
-    this.querySubscription = this.apollo.watchQuery<any>({
-      query: GET_CONFIGURATION
-    })
-      .valueChanges
-      .subscribe(({ data}) => {
-        this.config = data.config;
-        console.log('from subscribe in fetchconfig');
-        return this.config;
-      });
-      console.log('from outside subscribe in fetchconfig');
-      return null;
+@Injectable()
+export class ConfigService implements OnInit {
+  public config: ConfigModel;
+  constructor(private apollo: Apollo, private httpLink: HttpLink) {
+  }
+
+  ngOnInit() {
+  }
+
+  createApollo() {
+    this.apollo.create({
+      link: this.httpLink.create({ uri: 'http://localhost:8080/graphql'}),
+      cache: new InMemoryCache()
+    });
+    console.log('from constructor in appcomponent');
+    console.log(this.apollo.getClient());
+  }
+
+  getConfig() {
+    return this.config;
+  }
+
+  setConfig(config: ConfigModel) {
+    console.log(config);
+    this.config = config;
+  }
+  test() {
+    console.log('test');
   }
 }
 
